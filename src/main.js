8e6acaf91398df3ba562b7352abe0a65fca7f4ba
17879306628 <1941857998@qq.com>
2020-10-28 20:38:09 +0800

    第十一次提交

--- src/main.js
+++ src/main.js
@@ -5,11 +5,22 @@ import store from './store'
 
 import toast from 'components/common/toast'
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+
+// 安装toast插件
 Vue.use(toast)
 
 Vue.config.productionTip = false
-
+// 使用事件总线
 Vue.prototype.$bus = new Vue()
+// 使用移动端300ms延迟
+FastClick.attach(document.body)
+// 使用懒加载的插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   el: '#app',
